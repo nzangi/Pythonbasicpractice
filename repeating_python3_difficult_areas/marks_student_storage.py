0571af283876991c0f00daaaa7cd87f545b94c4e
nzangi <nzangimuoki>
2021-03-23 10:24:21 +0300

    done work

---
+++ repeating_python3_difficult_areas/marks_student_storage.py
@@ -0,0 +1,36 @@
+# names = []
+# subjects = []
+# marks = []
+#
+#
+# for i in range(5):
+#     name = input(f"Please enter the names of student {i + 1} : ")
+#     names.append(name)
+#     for j in range(5):
+#         subject = input("Enter the names of subject to calculate the sum and average: ")
+#         subjects.append(subject)
+#         for k in range(5):
+#             total = 0
+#             mark = int(input("Please  input the marks of each student accordingly to the subject: "))
+#             marks.append(mark)
+#
+names = []
+subjects = []
+marks = []
+total = 0
+avarage = 0
+
+number_of_students = input("Please enter the number of students you want to calculate their marks: ")
+for i in number_of_students:
+    name = input(f"Please enter the name of student : ")
+    names.append(name)
+    for k in range(5):
+        subject = input(f"Enter the names of subject to calculate the sum and average of {name}: ")
+        subjects.append(subject)
+        mark = int(input(f"Please  input the marks of {name} accordingly to the subject: "))
+        marks.append(mark)
+        total = sum(marks)
+        avarage = total/len(marks)
+
+for i in number_of_students:
+    print(names[i] + ":" + subjects[i] + ":" + marks[i]+ total  + ":" + avarage[i])
