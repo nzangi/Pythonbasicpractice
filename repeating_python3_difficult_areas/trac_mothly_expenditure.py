78e4b7904532be8281448d7c6b08f92e1f213dcb
nzangi <nzangimuoki>
2021-03-26 14:03:02 +0300

    work for today

--- repeating_python3_difficult_areas/trac_mothly_expenditure.py
+++ repeating_python3_difficult_areas/trac_mothly_expenditure.py
@@ -1,13 +1,14 @@
-house_hold = []
-name = input("Your name: ")
+# house_hold = []
+# name = input("Your name: ")
 months_in_year = [1,2,3,4,5,6,7,8,9,10,11,12]
 savings = []
 
 for month in months_in_year:
     monthly_expenditure = int(input(f"Enter your monthly expenditure in this year 1 in month {month}: "))
     savings_account = int(1/3 * monthly_expenditure)
-    savings.append(sum(savings_account))
-    print(savings)
+    savings.append(savings_account)
+
+    print(f"Your total savings is: {sum(savings)}")
 
 
 
